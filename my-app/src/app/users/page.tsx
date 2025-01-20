0749f3c25540c37cf901369d49506b1536a7e7cb
DarisMavric <mvrcdaris@gmail.com>
2025-01-20 16:19:17 +0100

    Created Form

---
+++ my-app/src/app/users/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import FormSubmit from "./FormSubmit";
+
+export default function Page(){
+    return (
+        <div className="w-[350px] h-[200px] p-3 bg-black text-white">
+            <FormSubmit/>
+        </div>
+    )
+}
