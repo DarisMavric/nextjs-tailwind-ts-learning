31b895fa9494b77b8df93bf74933d4ad5c33721f
DarisMavric <mvrcdaris@gmail.com>
2025-01-25 18:10:05 +0100

    Created Budgets Page And Create Budget Modal

---
+++ my-app/src/app/(routes)/dashboard/budgets/_components/CreateBudget.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import EmojiPicker from "emoji-picker-react"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+
+const CreateBudget = () => {
+    const [emojiIcon,setEmojiIcon] = useState('blush');
+    const [openEmojiPicker,setOpenEmojiPicker] = useState(false);
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="bg-slate-100 p-10 rounded-md items-center flex flex-col border-2 border-dashed cursor-pointer hover:shadow-md">
+            <h2 className="text-3xl">+</h2>
+            <h2>Create New Budget</h2>
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Budget</DialogTitle>
+            <DialogDescription>
+                <div className="mt-5">
+                    <Button variant='outline' className="text-lg" onClick={() => setOpenEmojiPicker(!openEmojiPicker)}>{emojiIcon}</Button>
+                    <div className="absolute">
+                        <EmojiPicker open={openEmojiPicker} onEmojiClick={(e) => {setEmojiIcon(e.emoji); setOpenEmojiPicker(false)}}/>
+                    </div>
+                    <div className="mt-2">
+                        <h2 className="text-black font-medium my-1">Budget Name</h2>
+                        <Input placeholder="e.g Home Decor"/>
+                    </div>
+                    <div className="mt-2">
+                        <h2 className="text-black font-medium my-1">Budget Amount</h2>
+                        <Input placeholder="e.g 5000$"/>
+                    </div>
+
+                    <Button className="mt-5 w-full">Create Budget</Button>
+                </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default CreateBudget;
--- my-app/src/app/(routes)/dashboard/layout.jsx
+++ my-app/src/app/(routes)/dashboard/layout.jsx
@@ -1,9 +1,33 @@
-import React from "react"
+"use client"
+
+import React, { useEffect } from "react"
 import SideNav from "./_components/SideNav";
 import DashboardHeader from "./_components/DashboardHeader";
+import { db } from '@/utils/dbConfig.ts';
+import { Budgets } from '@/utils/schema.ts';
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { useRouter } from "next/navigation";
 
 
 export default function DashboardLayout({children}){
+
+
+    const {user} = useUser();
+    const router = useRouter();
+    useEffect(() => {
+        user&&checkUserBudgets();
+    },[user])
+
+    const checkUserBudgets = async() => {
+        const result = await db.select().from(Budgets).where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAddress));
+
+
+        console.log(result);
+        if(result?.length==0){
+            router.replace('/dashboard/budgets')
+        }
+    }
     return (
         <div className="">
             <div className="fixed md:w-64 hidden md:block bg-red-100">
